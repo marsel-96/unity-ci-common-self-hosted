4d680a68e3712e219da153992a1a1ae39e5675e6
Marco Selleri <marco.selleri96@gmail.com>
2024-09-18 22:48:40 +0200

    Move index.ts to root dir

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { runCommand } from "./src/utils/command";
+export { logLines } from "./src/utils/log";
+export { getUnityPath } from "./src/utils/unity";
+export { validateVariables } from "./src/utils/validate";
+export { VariableValue, Variables } from "./src/model/variable";
