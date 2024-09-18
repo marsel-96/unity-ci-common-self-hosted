9cad03b7a24d1c984fdfbed453bb969c182287b7
Marco Selleri <marco.selleri96@gmail.com>
2024-09-18 22:42:22 +0200

    Add module exports directive

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { runCommand } from "./utils/command";
+import { logLines } from "./utils/log";
+import { getUnityPath } from "./utils/unity";
+import { validateVariables } from "./utils/validate";
+
+module.exports = {
+    validateVariables,
+    getUnityPath,
+    logLines,
+    runCommand
+}
