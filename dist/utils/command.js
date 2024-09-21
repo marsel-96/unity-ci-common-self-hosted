2c36854035f1bfdc733344714ad85fcacc6e62e8
Marco Selleri <marco.selleri96@gmail.com>
2024-09-21 12:43:41 +0200

    Fix wait stdout to be full consumed in runCommand

--- dist/utils/command.js
+++ dist/utils/command.js
@@ -2,14 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.runCommand = runCommand;
 const child_process_1 = require("child_process");
-function runCommand(command, args) {
+async function runCommand(command, args) {
     let child = (0, child_process_1.spawn)(command, args);
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
+    const waitOutputCompletion = new Promise((resolve, _) => {
+        child.stdout.on('end', () => {
+            resolve();
+        });
+    });
     return new Promise((resolve, reject) => {
         child.on('error', () => reject());
         child.on('close', (code) => resolve(code));
         child.on('exit', (code) => resolve(code));
+    })
+        .then(async (exit) => {
+        const timeoput = setTimeout(() => { }, 2000);
+        await Promise.any([
+            timeoput,
+            waitOutputCompletion
+        ]);
+        return exit;
     });
 }
 //# sourceMappingURL=command.js.map
--- src/utils/command.ts
+++ src/utils/command.ts
@@ -1,14 +1,28 @@
 import { spawn } from 'child_process';
 
-export function runCommand(command: string, args: string[]): Promise<number> {
+export async function runCommand(command: string, args: string[]): Promise<number> {
     let child = spawn(command, args)
 
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
 
+    const waitOutputCompletion = new Promise<void>( (resolve, _) => {
+        child.stdout.on('end', () => {
+            resolve();
+        })
+    }); 
+
     return new Promise<number>((resolve, reject) => {
         child.on('error', () => reject());
         child.on('close', (code: number) => resolve(code));
         child.on('exit', (code: number) => resolve(code));
+    })
+    .then(async (exit) => {
+        const timeoput = setTimeout(() => {}, 2000)
+        await Promise.any([
+            timeoput,
+            waitOutputCompletion
+        ])
+        return exit
     });
 }
